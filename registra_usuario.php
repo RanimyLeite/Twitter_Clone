5e446dcb9769b2c5b2ac5b51192f8bd6d21409c9
Ranimy Leite <ranimy.leitecosta@gmail.com>
2019-11-06 17:43:05 -0300

    Blocking already registered users (part - 1)

--- registra_usuario.php
+++ registra_usuario.php
@@ -1,4 +1,5 @@
 <?php
+	//Script que recebe dados do form de incrição e gurda no banco
 
 	require_once('db.config.php');
 
@@ -15,6 +16,42 @@
 	//Como a função conecta_mysql retorna um link de conexão(return $con) ela precisa ser recuperada de alguma forma e para isso atribuimos ela a  uma var $link;
 	$link = $objDb->conecta_mysql();
 
+	//Verificando se o usuario já existe
+	$sql = " select * from usuarios where usuario = '$usuario'";
+	if($resultado_id = mysqli_query($link, $sql)){
+
+		$dados_usuario = mysqli_fetch_array($resultado_id);
+
+		if(isset($dados_usuario)){
+			echo 'Usuário já cadastrado!!';
+		} else {
+			echo 'Usuário não cadastrado, OK! Pode cadastrar!';
+		}
+	} else {
+
+		echo 'Erro ao tentar localizar o registro do usuario';
+	}
+
+
+	//Verificando se o email já existe
+
+	$sql = " select * from usuarios where email = '$email'";
+	if($resultado_id = mysqli_query($link, $sql)){
+
+		$dados_usuario = mysqli_fetch_array($resultado_id);
+
+		if(isset($dados_usuario['email'])){
+			echo 'Email já cadastrado!!';
+		} else {
+			echo 'Email não cadastrado, OK! Pode cadastrar!';
+		}
+
+	} else {
+		echo 'Erro ao tentar localizar o registro de email';
+	}
+
+
+	die();//interrompe a execução a partir daqui
 
 	//Montando a Query(Instrução de insert dos dados)no banco
 	$sql = " insert into usuarios(usuario, email, senha) values ('$usuario', '$email', '$senha')";
